4c0ed19a2b9a1b7061f76e23e7a545a1dd7bd2ca
Anna Shchiptsova <shchipts@iiasa.ac.at>
2020-09-12 12:08:33 +0200

    version update: utilities-clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.iiasa/commodities-auction "0.2.1-SNAPSHOT"
+(defproject org.iiasa/commodities-auction "0.2.2-SNAPSHOT"
   :description "A special case of generalized English auction."
   :url "https://github.com/shchipts/commodities-auction"
   :scm {:name "git"
@@ -8,5 +8,5 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.iiasa/utilities-clj "1.1.0-SNAPSHOT"]]
+                 [org.iiasa/utilities-clj "1.2.0-SNAPSHOT"]]
   :main nil)
